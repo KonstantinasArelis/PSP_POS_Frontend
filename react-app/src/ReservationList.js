9f7675d538750b0320dc59bd42706495c1a9352a
KonstantinasArelis <arelis.konstantinas@gmail.com>
2024-11-07 14:38:32 +0200

    Added basic GET functionality for reservations

--- react-app/src/ReservationList.js
+++ react-app/src/ReservationList.js
@@ -4,8 +4,17 @@ const ReservationsList = ({reservations}) => {
         <div>
             {reservations.map(reservation =>(
                 <div key={reservation.id}>
-                    <h2>{reservation.id}</h2>
-                    <p>{reservation.total_amount}</p>
+                    <p>id: {reservation.id}</p>
+                    <p>business_id: {reservation.business_id}</p>
+                    <p>employee_id: {reservation.employee_id}</p>
+                    <p>client_name: {reservation.client_name}</p>
+                    <p>client_phone: {reservation.client_phone}</p>
+                    <p>created_at: {reservation.created_at}</p>
+                    <p>last_modified: {reservation.last_modified}</p>
+                    <p>appointment_time: {reservation.appointment_time}</p>
+                    <p>duration: {reservation.duration}</p>
+                    <p>ReservationStatus: {reservation.ReservationStatus}</p>
+                    <p>service_id: {reservation.service_id}</p>
                 </div>
             ))}
         </div>
--- react-app/src/Reservations.js
+++ react-app/src/Reservations.js
@@ -2,7 +2,8 @@ import ReservationsList from "./ReservationList";
 import useFetch from "./useFetch";
 
 const Reservations = () => {
-    const { error, isPending, data: reservations } = useFetch('http://localhost:5274/order?employee_id=123&min_total_amount=50&max_total_amount=1000&order_status=OPEN&page_nr=0&limit=25')
+    //const { error, isPending, data: reservations } = useFetch('http://localhost:5274/order?employee_id=123&min_total_amount=50&max_total_amount=1000&order_status=OPEN&page_nr=0&limit=25')
+    const { error, isPending, data: reservations } = useFetch('http://localhost:5274/Reservation')
 
     return(
         <div>
