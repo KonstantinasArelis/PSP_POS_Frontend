7e5d9f065a353722d34d03b5eca9daa4fce4bd66
KonstantinasArelis <arelis.konstantinas@gmail.com>
2024-12-04 14:34:32 +0200

    Added Business Editing UI

--- react-app/src/Business/BusinessAdminUser.js
+++ react-app/src/Business/BusinessAdminUser.js
@@ -1,9 +1,18 @@
 import { useEffect, useState } from "react";
 import SingleBusiness from "./SingleBusiness";
 import CreateBusinessPanel from "./CreateBusinessPanel";
+import EditBusinessPanel from "./EditBusinessPanel";
 
 const BusinessAdminUser = () => {
     const [data, SetData] = useState(null);
+    const [refreshTrigger, SetRefreshTrigger] = useState(0);
+
+    const handleRefresh = () => {
+        SetRefreshTrigger(prevTrigger => prevTrigger + 1);
+        console.log("trigger triggered");
+    }
+
+
 
     const url = `http://localhost:5274/Business`;
     useEffect(() => {
@@ -20,18 +29,22 @@ const BusinessAdminUser = () => {
         }).catch(err => {
             console.error("Error fetching business data", err);
         })
-    }, [])
+    }, [refreshTrigger])
 
 
     return(
         <div>
             <h2>Admin User Business View:</h2>
             <CreateBusinessPanel/>
-            {data && data.map(business => (
-                <div key={business.id}>
-                    <SingleBusiness Business={business}/>
-                </div>
-            ))}
+            <div className="businessList">
+                {data && data.map(business => (
+                    
+                    <div key={business.id}>
+                        <SingleBusiness Business={business}> </SingleBusiness>
+                        <EditBusinessPanel id={business.id} onRefresh = {handleRefresh}/>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
